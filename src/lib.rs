2d3cc6edf828ece6b7f789d4b650db2d74d3797c
Andrei <drop.all.requests@gmail.com>
2025-01-09 23:08:06 +0300

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,116 @@
+//! # multidict
+//!
+//! The `multidict` crate was inspired by Python `MultiDict` library
+//! `multidict` is useful for working with HTTP headers, URL query, form-data args etc
+//! HTTP Headers and URL query string require specific data structure: `multidict`.
+//! It behaves mostly like a regular map but it may have several values for the same key
+//! and preserves insertion ordering
+//!
+//! ## Examples
+//! ```no_run
+//! use multidict::MultiDict;
+//!
+//! let mut map = MultiDict::new();
+//! ```
+//!
+#[derive(Debug, Clone)]
+pub struct MultiDict {
+    pub elements: Vec<[String; 2]>,
+}
+impl MultiDict {
+    pub fn new() -> Self {
+        MultiDict {
+            elements: Vec::new(),
+        }
+    }
+
+    /// Creates new object with preset capacity
+    pub fn new_capacity(capacity: &usize) -> Self {
+        MultiDict {
+            elements: Vec::with_capacity(capacity.clone()),
+        }
+    }
+
+    /// Return the number of items in MultiDict
+    pub fn len(&self) -> usize {
+        self.elements.len()
+    }
+
+    /// Return the first key-value pair for key if key is in the MultiDict
+    pub fn get(&self, key: &str) -> Result<&[String; 2], &str> {
+        for item in &self.elements {
+            if item.get(0).unwrap().eq(key) {
+                return Ok(item);
+            }
+        }
+        Err("No matching key found for key")
+    }
+
+    /// If key is in the MultiDict, remove it and return its the first value, else return error
+    pub fn popone(&mut self, key: &str) -> Result<[String; 2], &str> {
+        for (idx, item) in self.elements.iter().enumerate() {
+            if item.get(0).unwrap().eq(key) {
+                return Ok(self.elements.remove(idx));
+            }
+        }
+        Err("No matching key found for key")
+    }
+
+    /// Return a list of all key-values for key if key is in the MultiDict
+    pub fn getall(&self, key: &str) -> Result<Vec<&[String; 2]>, &str> {
+        let mut results = Vec::new();
+        for item in &self.elements {
+            if item.get(0).unwrap().eq(key) {
+                results.push(item);
+            }
+        }
+        if !results.is_empty() {
+            Ok(results)
+        } else {
+            Err("No matching key found for key")
+        }
+    }
+
+    /// Return True if MultiDict has a key, else False.
+    pub fn contains(&self, key: &str) -> bool {
+        for item in &self.elements {
+            if item.get(0).unwrap().eq(key) {
+                return true;
+            }
+        }
+        false
+    }
+
+    /// Return Vec of all keys form MultiDict.
+    /// View contains all keys, possibly with duplicates.
+    pub fn keys(&self) -> Vec<&String> {
+        self.item_by_idx(0)
+    }
+
+    /// Return Vec of all values form MultiDict.
+    pub fn values(&self) -> Vec<&String> {
+        self.item_by_idx(1)
+    }
+
+    /// Update the MultiDict with the key/value pairs, overwriting existing keys
+    pub fn update(&self, key: &str) -> Result<Vec<&[String; 2]>, &str> {
+        let mut results = Vec::new();
+        for item in &self.elements {
+            if item.get(0).unwrap().eq(key) {
+                results.push(item);
+            }
+        }
+        if !results.is_empty() {
+            Ok(results)
+        } else {
+            Err("No matching key found for key")
+        }
+    }
+    fn item_by_idx(&self, idx: usize) -> Vec<&String> {
+        let mut results = Vec::with_capacity(self.elements.len());
+        for item in &self.elements {
+            results.push(item.get(idx).unwrap());
+        }
+        results
+    }
+}
